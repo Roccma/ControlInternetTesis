a8142667d451117c9e0e248d4dcf993bb9094f02
Unknown <mauroroccaf@gmail.com>
2018-07-28 00:54:43 -0300

    Commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const port = process.env.PORT || 3000;
+
+const fs = require('fs');
+
+var imgDir = `${__dirname}/img`;
+
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', (req, res) => {
+	res.redirect('index.html');
+});  
+
+app.get('/index', (req, res) => {
+	res.redirect('index.html');
+});
+
+app.get('/logs.txt', (req, res) => {
+	res.redirect('logs.txt');
+});
+
+app.get('/img/ftax.png', (req, res) => {
+	res.sendFile(`${imgDir}/ftax.png`);
+});
+
+io.on('connection', (socket) => {
+	socket.on('start', (fechaHora) => {
+		io.emit('start', {response : 'ok', fechaHora : fechaHora});
+		fs.appendFile('logs.txt', '[' + fechaHora + '] Inicio de conexión con el servidor\n');
+		//io.broadcast.emit('start', {response : 'ok', fechaHora : fechaHora});
+	});
+
+	socket.on('sinInternet', (fechaHora) => {
+		io.emit('sinInternet', {response : 'ok', fechaHora : fechaHora});
+		fs.appendFile('logs.txt', '[' + fechaHora + '] Celular sin conexión\n');
+	});
+
+	socket.on('reconexion', (fechaHora) => {
+		io.emit('reconexion', {response : 'ok', fechaHora : fechaHora});
+		fs.appendFile('logs.txt', '[' + fechaHora + '] El celular se ha reconectado\n');
+	});
+
+	socket.on('stop', (fechaHora) => {
+		io.emit('stop', {response : 'ok', fechaHora : fechaHora});
+		fs.appendFile('logs.txt', '[' + fechaHora + '] Fin de la prueba\n\n');
+		//io.broadcast.emit('stop', {response : 'ok', fechaHora : fechaHora});
+	});
+});
+
+http.listen(port, () => {
+	console.log("Servidor escuchando en el puerto " + port + "!");
+});
